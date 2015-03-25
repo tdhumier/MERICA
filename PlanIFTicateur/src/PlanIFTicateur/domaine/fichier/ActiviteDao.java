348fafc9d52d5af617fad6adba016c59bc58f188
Martin Deligny <rojowstyle@gmail.com>
2015-03-25 12:11:46 -0400

    fichierDao

---
+++ PlanIFTicateur/src/PlanIFTicateur/domaine/fichier/ActiviteDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PlanIFTicateur.domaine.fichier;
+
+/**
+ *
+ * @author martindeligny1
+ */
+public class ActiviteDao {
+    
+}
