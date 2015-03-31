17d947a341c02b94481193db30d77ca02d7f607b
Tristan D'humières <tristan.dhumieres@gmail.com>
2015-03-31 19:14:41 -0400

    séparation en deux méthodes de importerFichier()

--- PlanIFTicateur/src/PlanIFTicateur/domaine/fichier/GestionnaireFichier.java
+++ PlanIFTicateur/src/PlanIFTicateur/domaine/fichier/GestionnaireFichier.java
@@ -5,7 +5,6 @@
  */
 package PlanIFTicateur.domaine.fichier;
 
-import PlanIFTicateur.domaine.Horaire;
 import PlanIFTicateur.domaine.ListeActivites;
 import PlanIFTicateur.domaine.ListeGrillesCheminement;
 import java.io.File;
@@ -16,21 +15,22 @@ import java.io.File;
  */
 public class GestionnaireFichier {
 
-    public void chargementFichier(Horaire horaire, String adresseActivite) { // Lancer la récupération des données à partir de l'emplacement du fichier .COU passé en string
+    private String addresseFichierCOU;
 
-        System.out.println("Dans GestionnaireFichier / ChargementFichier");
-
-        File fichierActivite = new File(adresseActivite);
-        ActiviteDao activiteDao = new ActiviteDao(fichierActivite);
-        ListeActivites listeActivites = activiteDao.importerActivites();
-
-        String adresseCheminement = adresseActivite.substring(0, adresseActivite.length() - 4) + ".CHE"; // on retire les 4 derniers caractères (.COU) de l'adresse du fichier activite pour ajouter la fin du fichier .CHE
-        File fichierCheminement = new File(adresseCheminement);
-        CheminementDao cheminementDao = new CheminementDao(fichierCheminement);
-        ListeGrillesCheminement listeGrillesCheminement = cheminementDao.importerGrillesCheminement(listeActivites);
-
-        horaire.setGrillesCheminement(listeGrillesCheminement);
-        horaire.setListeActivite(listeActivites);
+    public GestionnaireFichier(String addresseFichierCOU) {
+        this.addresseFichierCOU = addresseFichierCOU;
     }
 
+    public ListeActivites getListeActivites() {
+        File fichierActivite = new File(addresseFichierCOU);
+        ActiviteDao activiteDao = new ActiviteDao(fichierActivite);
+        return activiteDao.importerActivites();
+    }
+
+    public ListeGrillesCheminement getGrillesCheminement(ListeActivites listeActivites) {
+        String adresseCheminement = addresseFichierCOU.substring(0, addresseFichierCOU.length() - 4) + ".CHE"; // on retire les 4 derniers caractères (.COU) de l'adresse du fichier activite pour ajouter la fin du fichier .CHE
+        File fichierCheminement = new File(adresseCheminement);
+        CheminementDao cheminementDao = new CheminementDao(fichierCheminement);
+        return cheminementDao.importerGrillesCheminement(listeActivites);
+    }
 }
