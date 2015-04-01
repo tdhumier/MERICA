aedd8df05baa15a771dd970984a892f8bca84760
Tristan D'humières <tristan.dhumieres@gmail.com>
2015-04-01 15:25:12 -0400

    méthode importerFichiers HoraireActiviteControleur

--- PlanIFTicateur/src/PlanIFTicateur/domaine/HoraireActiviteControleur.java
+++ PlanIFTicateur/src/PlanIFTicateur/domaine/HoraireActiviteControleur.java
@@ -4,47 +4,51 @@
  * and open the template in the editor.
  */
 package PlanIFTicateur.domaine;
+
 import PlanIFTicateur.domaine.activite.Activite;
-import java.util.List;
+import PlanIFTicateur.domaine.fichier.GestionnaireFichier;
 import java.awt.Point;
+import java.util.List;
 
 /**
  *
  * @author Alexandre
  */
-public class HoraireActiviteControleur 
-{
+public class HoraireActiviteControleur {
+
     private Horaire horaire;
-    
-    public enum ActiviteModes
-    {
+
+    public enum ActiviteModes {
+
         CoursClasse, CoursDistance, CoursHorsDep, Laboratoire
     }
-    
-    public HoraireActiviteControleur(Horaire horaire)
-    {
+
+    public HoraireActiviteControleur(Horaire horaire) {
         this.horaire = horaire;
     }
-    
-    public HoraireActiviteControleur()
-    {
+
+    public HoraireActiviteControleur() {
         horaire = new Horaire();
     }
-    
-    
-    public void deplacerActivite(int idActivite, Point mousePoint)
-    {
-        
+
+    public void deplacerActivite(int idActivite, Point mousePoint) {
+
     }
-    
-    public List<Activite> getActiviteListe()
-    {
+
+    public List<Activite> getActiviteListe() {
         return horaire.getListeActivite().getListeActivites();
     }
-    
-    public int getNombreActivite()
-    {
+
+    public int getNombreActivite() {
         return horaire.getListeActivite().getListeActivites().size();
     }
-    
+
+    public void importerFichiers(String path) {
+        GestionnaireFichier gestionnaireFichier = new GestionnaireFichier(path);
+        ListeActivites listeActivites = gestionnaireFichier.getListeActivites();
+        ListeGrillesCheminement listeGrillesCheminement = gestionnaireFichier.getGrillesCheminement(listeActivites);
+        horaire.setListeActivite(listeActivites);
+        horaire.setGrillesCheminement(listeGrillesCheminement);
+    }
+
 }
