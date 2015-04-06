65c4509c482aa49de63366a2de0927cfca6db47b
Alexandre Rivest <rivest123@hotmail.com>
2015-04-06 01:06:33 -0400

    Couleur du menu et detail d'activité en developpement

--- PlanIFTicateur/src/PlanIFTicateur/domaine/activite/CoursClasse.java
+++ PlanIFTicateur/src/PlanIFTicateur/domaine/activite/CoursClasse.java
@@ -7,19 +7,21 @@ package PlanIFTicateur.domaine.activite;
 
 import java.awt.Color;
 
+
 /**
  *
  * @author Alexandre
  */
 public class CoursClasse extends Activite {
 
-    private Color couleur;
+   
     private String type;
+    private Color couleur = new Color(255, 0, 0, 50);
 
     public CoursClasse(String code, String section, String titre, String professeur, String type, double duree, double heureDebutMin, double heureFinMax, int jour, double heureDebut) {
         super(code, section, titre, professeur, type, duree, heureDebutMin, heureFinMax, jour, heureDebut);
         this.type = "CoursClasse";
-        this.couleur = Color.RED;
+        this.couleur = couleur;
         int i = 0;
     }
 
