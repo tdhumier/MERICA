395b199804b6f21700fdabc435a614021290971a
Tristan D'humières <tristan.dhumieres@gmail.com>
2015-04-06 12:07:31 -0400

    ListTransferHandler in comment

--- PlanIFTicateur/src/PlanIFTicateur/gui/RightPanel.java
+++ PlanIFTicateur/src/PlanIFTicateur/gui/RightPanel.java
@@ -9,21 +9,15 @@ import PlanIFTicateur.domaine.activite.Activite;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Transferable;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.Serializable;
 import java.util.List;
 import javax.swing.DefaultListModel;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
-import javax.swing.TransferHandler;
 
 import javax.swing.border.BevelBorder;
 import javax.swing.event.ListSelectionEvent;
@@ -56,7 +50,7 @@ public class RightPanel extends JPanel implements Serializable {
     private JLabel finMaxLabel;
     private JLabel jourLabel;
     private JLabel heureLabel;
-    
+
     public RightPanel() {
     }
 
@@ -85,23 +79,18 @@ public class RightPanel extends JPanel implements Serializable {
         }
         listeActivites = new JList(listModel);
         listeActivites.setCellRenderer(new ActiviteRenderer());
-       
-        
-        
-        listeActivites.addListSelectionListener(new ListSelectionListener()
-                {
+
+        listeActivites.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 modifierLabel(listeActivites.getSelectedValue());
             }
-                });
-        
+        });
+
         listeActivites.setDragEnabled(true);
         listeActivites.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        listeActivites.setTransferHandler(new ListTransferHandler());
-        
-        
-        
+        //listeActivites.setTransferHandler(new ListTransferHandler());
+
         listeActivitesPanel.add(new JScrollPane(listeActivites), BorderLayout.CENTER);
         add(listeActivitesPanel);
 
@@ -109,9 +98,9 @@ public class RightPanel extends JPanel implements Serializable {
 
         detailsActiviteLabel = new JLabel("\t Détails Activité \n\n");
         detailsActiviteLabel.setHorizontalAlignment(SwingConstants.CENTER);
-       
-        detailsActivitePanel = new JPanel(new GridLayout(10,1));   
-        
+
+        detailsActivitePanel = new JPanel(new GridLayout(10, 1));
+
         codeLabel = new JLabel("Code activité : ");
         sectionLabel = new JLabel("Section : ");
         titreLabel = new JLabel("Titre : ");
@@ -122,25 +111,22 @@ public class RightPanel extends JPanel implements Serializable {
         finMaxLabel = new JLabel("Fin maximal possible : ");
         jourLabel = new JLabel("Jour : ");
         heureLabel = new JLabel("Heure  : ");
-        
-        detailsActivitePanel.add( codeLabel);
+
+        detailsActivitePanel.add(codeLabel);
         detailsActivitePanel.add(titreLabel);
         detailsActivitePanel.add(sectionLabel);
-        detailsActivitePanel.add(profLabel); 
+        detailsActivitePanel.add(profLabel);
         detailsActivitePanel.add(typeLabel);
         detailsActivitePanel.add(dureeLabel);
         detailsActivitePanel.add(debutMinLabel);
         detailsActivitePanel.add(finMaxLabel);
-        detailsActivitePanel.add( jourLabel);
+        detailsActivitePanel.add(jourLabel);
         detailsActivitePanel.add(heureLabel);
-                   
+
         rightCenterPanel.add(detailsActiviteLabel, BorderLayout.PAGE_START);
         rightCenterPanel.add(detailsActivitePanel, BorderLayout.CENTER);
-        
+
         add(rightCenterPanel);
-      
-        
-        
 
         statistiquePanel = new JPanel();
         statistiqueLabel = new JLabel("Statistiques");
@@ -151,17 +137,14 @@ public class RightPanel extends JPanel implements Serializable {
         setVisible(true);
     }
 
-    
-    
     public void miseAjourListe() {
         List<Activite> activites = mainWindow.controleur.getActivitesNonAssignees();
         activites.stream().filter((activite) -> (!listModel.contains(activite))).forEach((activite) -> {
             listModel.addElement(activite);
         });
     }
-    
-    public void modifierLabel(Activite activite)
-    {
+
+    public void modifierLabel(Activite activite) {
         codeLabel.setText("Code activité : " + activite.getCode());
         sectionLabel.setText("Section : " + activite.getSection());
         titreLabel.setText("Titre : " + activite.getTitre());
@@ -170,17 +153,16 @@ public class RightPanel extends JPanel implements Serializable {
         dureeLabel.setText("Durée : " + activite.getDuree() + "heures");
         debutMinLabel.setText("Début minimal possible : " + activite.getHeureDebutMin() + " heures");
         finMaxLabel.setText("Fin maximal possible : " + activite.getHeureFinMax() + " heures");
-        if(activite.getJour() != 0 || activite.getHeureDebut() != 0)
-        {
+        if (activite.getJour() != 0 || activite.getHeureDebut() != 0) {
             jourLabel.setText("Jour : " + getNomJour(activite.getJour()));
             heureLabel.setText("Heure  : " + activite.getHeureDebut());
         }
     }
-    public String getNomJour(int i)
-    {
-        String[] JourModes= {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"};
-      
-        return JourModes[i+1];
+
+    public String getNomJour(int i) {
+        String[] JourModes = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"};
+
+        return JourModes[i + 1];
     }
-    
+
 }
