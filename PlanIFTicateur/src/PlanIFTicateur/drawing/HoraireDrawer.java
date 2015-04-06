13efa0b94ecef7f46d56c8803189739d13cf3bf9
Tristan D'humières <tristan.dhumieres@gmail.com>
2015-04-06 00:41:18 -0400

    encore la grille

--- PlanIFTicateur/src/PlanIFTicateur/drawing/HoraireDrawer.java
+++ PlanIFTicateur/src/PlanIFTicateur/drawing/HoraireDrawer.java
@@ -7,9 +7,12 @@ package PlanIFTicateur.drawing;
 
 import PlanIFTicateur.domaine.HoraireActiviteControleur;
 import PlanIFTicateur.domaine.activite.Activite;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.util.List;
 
 /**
@@ -36,16 +39,36 @@ public class HoraireDrawer {
         int hauteurCase = (int) (initialDimension.height - 30) / 48;
         g.setColor(Color.LIGHT_GRAY);
 
+        Graphics2D g2 = (Graphics2D) g;
+        Stroke s = g2.getStroke();
+
+        //Dessine les lignes verticale
         for (int x = 0; x <= 28; x++) {
-            g.drawLine(x * largeurCase + 80, 20, x * largeurCase + 80, hauteurCase * 48 + 20);
+            g2.setStroke(s);
+            if (x % 2 == 0) {
+                g2.setStroke(new BasicStroke(2));
+            }
+            g2.drawLine(x * largeurCase + 80, 20, x * largeurCase + 80, hauteurCase * 48 + 20);
         }
 
+        g2.setStroke(new BasicStroke(2));
+        g2.drawLine(10, 20, 10, hauteurCase * 48 + 20);
+
+        //Dessine les lignes horizontale
         for (int y = 0; y <= 48; y++) {
-            g.drawLine(80, y * hauteurCase + 20, largeurCase * 28 + 80, y * hauteurCase + 20);
+            g2.setStroke(s);
+            if (y % 8 == 0) {
+                g2.setStroke(new BasicStroke(2));
+                g2.drawLine(10, y * hauteurCase + 20, largeurCase * 28 + 80, y * hauteurCase + 20);
+            } else {
+                g2.drawLine(80, y * hauteurCase + 20, largeurCase * 28 + 80, y * hauteurCase + 20);
+            }
+
         }
-        //g.drawLine(largeur - 10, 10, largeur - 10, hauteur - 10);
+
+        //Affiche les heures
         for (int i = 8; i <= 22; i++) {
-            g.drawString(i + "h", 70 + 2 * largeurCase * (i - 8), 15);
+            g2.drawString(i + "h", 70 + 2 * largeurCase * (i - 8), 15);
         }
     }
 
