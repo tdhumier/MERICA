3d958a7cf1cc62e9ec90993c80c2d0fe17fdb090
Martin Deligny <rojowstyle@gmail.com>
2015-04-15 14:51:22 -0400

    Export

--- PlanIFTicateur/src/PlanIFTicateur/gui/frames/MainWindow.java
+++ PlanIFTicateur/src/PlanIFTicateur/gui/frames/MainWindow.java
@@ -81,6 +81,7 @@ public class MainWindow extends javax.swing.JFrame {
         jSeparator1 = new javax.swing.JPopupMenu.Separator();
         sauvegarderMenuItem = new javax.swing.JMenuItem();
         sauvegarderSousMenuItem = new javax.swing.JMenuItem();
+        exportMenuItem = new javax.swing.JMenuItem();
         jSeparator2 = new javax.swing.JPopupMenu.Separator();
         quitterMenuItem = new javax.swing.JMenuItem();
 
@@ -185,6 +186,14 @@ public class MainWindow extends javax.swing.JFrame {
             }
         });
         menuFichier.add(sauvegarderSousMenuItem);
+
+        exportMenuItem.setText("Exporter au format image");
+        exportMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exportMenuItemActionPerformed(evt);
+            }
+        });
+        menuFichier.add(exportMenuItem);
         menuFichier.add(jSeparator2);
 
         quitterMenuItem.setText("Quitter");
@@ -278,11 +287,29 @@ public class MainWindow extends javax.swing.JFrame {
         File fichier;
         if (dialogue.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             fichier = dialogue.getSelectedFile();
+
             System.out.println("absolute path:" + fichier.getAbsolutePath());
             System.out.println("filename:" + fichier.getName());
-            controleur.enregistrerFichier(controleur.getActivites(), fichier.getAbsolutePath());
+            //controleur.enregistrerFichier(controleur.getActivites(), fichier.getAbsolutePath());
         }
     }//GEN-LAST:event_sauvegarderSousMenuItemActionPerformed
+
+    private void exportMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exportMenuItemActionPerformed
+        JFileChooser dialogue = new JFileChooser(new File("/"));
+        dialogue.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        if (dialogue.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            int w = horairePanel.getWidth();
+            int h = horairePanel.getHeight();
+            BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = bi.createGraphics();
+            horairePanel.paint(g);
+            try {
+                ImageIO.write(bi, "png", new File(dialogue.getSelectedFile() + "/horaire.png"));
+            } catch (IOException ex) {
+                Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }//GEN-LAST:event_exportMenuItemActionPerformed
 
     /**
      * @param args the command line arguments
@@ -329,6 +356,7 @@ public class MainWindow extends javax.swing.JFrame {
     private javax.swing.JPanel boutonTopPanel;
     private javax.swing.JPanel centerPanel;
     private javax.swing.JButton exportBouton;
+    private javax.swing.JMenuItem exportMenuItem;
     public PlanIFTicateur.gui.panels.HorairePanel horairePanel;
     private javax.swing.JScrollPane horaireScrollPane;
     private javax.swing.JPopupMenu.Separator jSeparator1;
